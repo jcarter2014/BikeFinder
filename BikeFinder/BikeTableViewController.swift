76dfebf20ad7ff63c2e83c1134046e00f27330c1
Jack Carter <jcarter2014@gmail.com>
2018-05-11 18:25:43 +0900

    Initial Commit

---
+++ BikeFinder/BikeTableViewController.swift
@@ -0,0 +1,114 @@
+//
+//  BikeTableViewController.swift
+//  BikeFinder
+//
+//  Created by John Carter on 2/5/18.
+//  Copyright © 2018 Jack Carter. All rights reserved.
+//
+
+import UIKit
+
+class BikeTableViewController: UITableViewController {
+    
+    var dict: [String: Double] = [:]
+    var sortedDistances = [(key: String, value: Double)]()
+    var distances = [Double]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let tab1Controller = self.tabBarController?.viewControllers?.first as! ViewController
+        
+        self.dict = tab1Controller.stationDistances
+        self.sortedDistances = self.dict.sorted(by: { $0.value < $1.value })
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    /*override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }*/
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return dict.count
+    }
+    
+    @IBAction func test(_ sender: Any) {
+        print(dict)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+        cell.stationName.text = sortedDistances[indexPath.row].key
+        cell.distance.text = String(sortedDistances[indexPath.row].value)
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BikeFinder/ViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  BikeFinder
+//
+//  Created by John Carter on 1/9/18.
+//  Copyright © 2018 Jack Carter. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    var stationQuantity: [String: Any] = [:]
+    var stationDocks: [String: Any] = [:]
+    var stationDistances: [String: Double] = [:]
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        
+        map.showsUserLocation = true
+        
+    }
+    
+    
+    @IBAction func getCapitalBikeLocations() {
+        
+        Alamofire.request("https://gbfs.capitalbikeshare.com/gbfs/en/station_information.json").responseJSON { (response) in
+            
+            if let JSON = response.result.value as? [String: Any] {
+                
+                if let data = JSON["data"] as? [String: Any] {
+                    
+                    if let stations = data["stations"] as? [[String: Any]] {
+                        
+                        for eachItem in stations {
+
+                            let annotation = MKPointAnnotation()
+                            annotation.coordinate.latitude = eachItem["lat"] as! CLLocationDegrees
+                            annotation.coordinate.longitude = eachItem["lon"] as! CLLocationDegrees
+                            
+                            let stationID = eachItem["station_id"] as! String
+                            
+                            let numBikes = self.stationQuantity[stationID]!
+                            let numBikesToString = String(describing: numBikes)
+                            
+                            annotation.title = numBikesToString
+                            let mySpot = self.manager.location
+                            let annotationSpot = CLLocation(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
+                            let distance = mySpot?.distance(from: annotationSpot)
+                            
+                            let stationName = eachItem["name"] as! String
+                            self.stationDistances[stationName] = distance!
+                            
+                            self.map.addAnnotation(annotation)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func getCapitalBikeQuantities() {
+        
+        Alamofire.request("https://gbfs.capitalbikeshare.com/gbfs/en/station_status.json").responseJSON { (response) in
+            if let JSON = response.result.value as? [String: Any] {
+                
+                if let data = JSON["data"] as? [String: Any] {
+                    
+                    if let stations = data["stations"] as? [[String: Any]] {
+                        for eachItem in stations {
+                            
+                            let stationID = eachItem["station_id"] as! String
+
+                            let numBikes = eachItem["num_bikes_available"]
+                            let numDocks = eachItem["num_docks_available"]
+                            
+                            self.stationQuantity[stationID] = numBikes
+                            self.stationDocks[stationID] = numDocks
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.1, 0.1)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(38.922, -77.076)
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        map.setRegion(region, animated: true)
+        map.showsUserLocation = true
+
+        getCapitalBikeLocations()
+        getCapitalBikeQuantities()
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func printStationDocks(_ sender: Any) {
+        print(stationDocks)
+    }
+    
+    @IBAction func printStationQuantities(_ sender: Any) {
+        print(stationQuantity)
+    }
+    
+    @IBAction func printStationDistances(_ sender: Any) {
+        print(stationDistances)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
